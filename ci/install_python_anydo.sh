fc39f01b3c3f13b4bf051332d3b2948d8b672cce
Dustin Brown <dbrown@jamasoftware.com>
2017-03-30 10:45:53 -0700

    use http for git clone idiot

--- ci/install_python_anydo.sh
+++ ci/install_python_anydo.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 
-git clone --depth=50 --branch=master git@github.com:dustinbrown/python-anydo.git dustinbrown/python-anydo
+git clone --depth=50 --branch=master https://github.com/dustinbrown/python-anydo.git dustinbrown/python-anydo
 
 cd dustinbrown/python-anydo
 pip install .
