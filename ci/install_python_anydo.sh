05c4a7574859c8d53eb93ab1e39e56e6c0157517
Dustin Brown <dbrown@jamasoftware.com>
2017-03-30 10:59:40 -0700

    install python-anydo locally

--- ci/install_python_anydo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -e
-
-git clone --depth=50 --branch=master https://github.com/dustinbrown/python-anydo.git dustinbrown/python-anydo
-
-cd dustinbrown/python-anydo
-pip install .
